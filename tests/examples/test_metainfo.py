0696502401fc74a0b460ed3a37acf9f2b378cff9
Markus Scheidgen <markus.scheidgen@gmail.com>
2025-02-18 18:04:18 +0000

    Added implementation and documentation for DataFrames.
    Changelog: Added

---
+++ tests/examples/test_metainfo.py
@@ -0,0 +1,16 @@
+import runpy
+import pytest
+import os
+
+
+prefix = os.path.join(__file__, '../../../examples/metainfo')
+
+
+@pytest.mark.parametrize(
+    'file',
+    [
+        f'{prefix}/data_frames.py',
+    ],
+)
+def test_metainfo(file):
+    runpy.run_path(file)
