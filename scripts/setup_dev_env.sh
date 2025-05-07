4cabfdaa0fb1caa4181b5e43627c5a7afe13fb03
Sascha Klawohn <sascha.klawohn@physik.hu-berlin.de>
2025-05-07 11:56:53 +0200

    Remove docs from setup script

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -8,7 +8,6 @@ project_dir=$(dirname $(dirname $(realpath $0)))
 cd $project_dir
 
 # Clean up any previous build
-rm -rf nomad/app/static/docs
 rm -rf nomad/app/static/gui
 rm -rf site
 
@@ -23,12 +22,6 @@ uv pip install -e ".[infrastructure,parsing,dev]" -c requirements-dev.txt
 # Install "default" plugins. TODO: This can be removed once we have proper
 # distributions projects.
 uv pip install -r requirements-plugins.txt # remove this after moving to distro
-
-# Build documentation
-sh scripts/generate_docs_artifacts.sh
-mkdocs build
-mkdir -p nomad/app/static/docs
-cp -r site/* nomad/app/static/docs
 
 # Generate .env file for the GUI
 python -m nomad.cli dev gui-env > gui/.env.development
