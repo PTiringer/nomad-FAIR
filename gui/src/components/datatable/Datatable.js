1983536bb2ece34d4ee0dd9242cb6f339a84224e
David Salamanca <juan.salamanca@physik.hu-berlin.de>
2025-07-10 09:36:12 +0000

    Improve data table empty rows fill

--- gui/src/components/datatable/Datatable.js
+++ gui/src/components/datatable/Datatable.js
@@ -473,7 +473,7 @@ export const DatatableTable = React.memo(function DatatableTable({children, acti
   const classes = useDatatableTableStyles()
   const {withSelectionFeature, multiSelect, getId} = useStaticDatatableContext()
   const {shownColumns, data, pagination, onPaginationChanged, selected, isLoading} = useDatatableContext()
-  const {page_size} = pagination
+  const {page_size, page} = pagination
   const emptyRows = Math.max(0, page_size - data.length)
   const columns = shownColumns
   const numberOfColumns = columns.length + (withSelectionFeature && multiSelect ? 1 : 0) + (actions ? 1 : 0)
@@ -529,8 +529,8 @@ export const DatatableTable = React.memo(function DatatableTable({children, acti
           </TableCell>
         </TableRow>
       )}
-      {!isExtending && (emptyRows > 0) && (
-        <TableRow style={{ height: 53 * emptyRows }}>
+      {!isExtending && (emptyRows > 0) && (page > 1 || data.length < 5) && (
+        <TableRow style={{ height: 53 * ((data.length < 5 && page === 1) ? 5 - data.length : emptyRows) }}>
           <TableCell colSpan={numberOfColumns} />
         </TableRow>
       )}
