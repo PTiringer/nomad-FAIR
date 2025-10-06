4b2e3b310c2ae4a0704efc4d0397fab24657c0ac
Lauri Himanen <lauri.himanen@physik.hu-berlin.de>
2025-10-06 12:20:04 +0000

    Added initial support for storing and querying search quantities based on the used definitions.

--- tests/variables.py
+++ tests/variables.py
@@ -1,5 +1,5 @@
 from nomad.metainfo.elasticsearch_extension import yaml_prefix
 
 yaml_schema_root = f'{yaml_prefix}-RnxU4MY59m1jtoAC4xcDiFkfy58'
-yaml_schema_name = f'{yaml_schema_root}.MySChema'
+yaml_schema_name = f'{yaml_schema_root}.MySchema'
 python_schema_name = 'nomadschemaexample.schema.MySchema'
