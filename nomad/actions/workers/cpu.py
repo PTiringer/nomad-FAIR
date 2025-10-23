0d4e1c2e87119565f94bcac16bad8de22f2c6f4d
Lauri Himanen <lauri.himanen@physik.hu-berlin.de>
2025-10-23 09:35:03 +0000

    Merge branch 'processpool-default-worker' into 'develop'
    Use ProcessPoolExecutor for processing
    
    See merge request nomad-lab/nomad-FAIR!2730

--- nomad/actions/workers/cpu.py
+++ nomad/actions/workers/cpu.py
@@ -4,11 +4,12 @@ from concurrent.futures import ThreadPoolExecutor
 from nomad.actions import TaskQueue
 from nomad.actions.client import get_client
 from nomad.actions.workers.util import get_worker
+from nomad.infrastructure import setup
 
 
 async def run_worker():
     client = await get_client()
-    with ThreadPoolExecutor(max_workers=12) as executor:
+    with ThreadPoolExecutor(max_workers=12, initializer=setup) as executor:
         worker = get_worker(
             client=client, task_queue=TaskQueue.CPU, activity_executor=executor
         )
