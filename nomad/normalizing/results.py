0f9d5b978e3b4897533d8d7154eb6ee14fcf7cd2
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-12-02 11:51:26 +0000

    Remove dependency of results datamodel on simulationworkflowschema.
    Changelog: Changed

--- nomad/normalizing/results.py
+++ nomad/normalizing/results.py
@@ -858,8 +858,8 @@ class ResultsNormalizer(Normalizer):
             energies = thermo_prop.vibrational_free_energy_at_constant_volume
             if valid_array(temperatures) and valid_array(energies):
                 energy_free = EnergyFreeHelmholtz()
-                energy_free.energies = thermo_prop
-                energy_free.temperatures = thermo_prop
+                energy_free.energies = energies
+                energy_free.temperatures = temperatures
                 return energy_free
 
         return None
@@ -884,8 +884,8 @@ class ResultsNormalizer(Normalizer):
             heat_capacities = thermo_prop.heat_capacity_c_v
             if valid_array(temperatures) and valid_array(heat_capacities):
                 heat_cap = HeatCapacityConstantVolume()
-                heat_cap.heat_capacities = thermo_prop
-                heat_cap.temperatures = thermo_prop
+                heat_cap.heat_capacities = heat_capacities
+                heat_cap.temperatures = temperatures
                 return heat_cap
 
         return None
@@ -905,7 +905,7 @@ class ResultsNormalizer(Normalizer):
                         geo_opt.system_optimized = (
                             workflow.results.calculation_result_ref.system_ref
                         )
-                    geo_opt.energies = workflow.results
+                    geo_opt.energies = workflow.results.energies
                     geo_opt.final_energy_difference = (
                         workflow.results.final_energy_difference
                     )
@@ -1415,8 +1415,8 @@ class ResultsNormalizer(Normalizer):
             ev_curves.append(
                 EnergyVolumeCurve(
                     type='raw',
-                    volumes=workflow.results,
-                    energies_raw=workflow.results,
+                    volumes=volumes,
+                    energies_raw=energies_raw,
                 )
             )
         else:
@@ -1433,8 +1433,8 @@ class ResultsNormalizer(Normalizer):
                 ev_curves.append(
                     EnergyVolumeCurve(
                         type=function_name,
-                        volumes=workflow.results,
-                        energies_fit=fit,
+                        volumes=volumes,
+                        energies_fit=energies_fitted,
                     )
                 )
 
