e7a92d194d1dcc5a0e5fee0bd62c70548df76479
Haoyu Yang <yanghaoyu97@outlook.com>
2025-12-08 11:36:54 +0100

    remove leftover `parsing` group in extras

--- scripts/generate_python_dependencies.sh
+++ scripts/generate_python_dependencies.sh
@@ -16,13 +16,13 @@ cd $project_dir
 
 # Compile requirements for a production installation
 uv pip compile -U --universal -p 3.10 --annotation-style=line \
-    --extra=infrastructure --extra=parsing \
+    --extra=infrastructure \
     --output-file=requirements.txt \
     pyproject.toml
 
 # Compile requirements for a dev installation
 uv pip compile -U --universal -p 3.10 --annotation-style=line \
-    --extra=dev --extra=infrastructure --extra=parsing \
+    --extra=dev --extra=infrastructure \
     --output-file=requirements-dev.txt \
     requirements.txt \
     pyproject.toml
