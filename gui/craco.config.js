21c15d6cf78fb28e77c1915ab3781f8bb7141edf
PTiringer <peter.tiringer@gmail.com>
2026-05-27 16:27:10 +0200

    craco fix

--- gui/craco.config.js
+++ gui/craco.config.js
@@ -6,6 +6,23 @@ module.exports = {
   }],
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.resolve.alias = {
+        ...(webpackConfig.resolve.alias || {}),
+        'fraction.js$': require.resolve('fraction.js')
+      }
+
+      const cjsRule = {
+        test: /\.cjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      }
+      const oneOfRule = webpackConfig.module.rules.find((rule) => Array.isArray(rule.oneOf))
+      if (oneOfRule) {
+        oneOfRule.oneOf.unshift(cjsRule)
+      } else {
+        webpackConfig.module.rules.push(cjsRule)
+      }
+
       // Add a rule to handle .mjs files
       webpackConfig.module.rules.push({
         test: /\.mjs$/,
